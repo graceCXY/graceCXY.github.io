1169f4fff84cfe8e789d605671bdbed9065bb5e2
graceCXY <40971474+graceCXY@users.noreply.github.com>
2020-08-05 00:30:09 -0700

    Add files via upload
    Added animation/transition and linked projects and writings

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// let bio = document.getElementById('bio');
-// bio.onmouseover = function() {
-//     bio.style.innerHTML = 'HIIIII';
-//     bio.style.backgroundColor = 'green';
-// }
-
-// var projs = document.getElementById("projects").querySelectorAll(".proj");
-// let proj2 = document.getElementById('proj2');
-// let proj3 = document.getElementById('proj-three');
-
-// $(document).ready(function(){
-//     $('.menu').click(function(){
-//         $('ul').toggleClass('active');
-//     })
-// })
 
 function showProjDescription(img, description) {
     document.getElementById(description).style.display = "block";
@@ -24,17 +9,17 @@ function hideProjDescription(img, description){
     document.getElementById(img).style.display = "block";
 };
 
-
 function filterSelection(c) {
     var x, i;
     x = document.getElementsByClassName("project");
     if (c == "all") c = "";
     for (i = 0; i < x.length; i++) {
-        // x[i].innerHTML = "hiiiii";
         removeClass(x[i], "show");
         if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
     }
 }
+
+
 
 function addClass(element, name) {
     var i, arr1, arr2;
@@ -44,7 +29,6 @@ function addClass(element, name) {
     if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
     }
 }
-
 function removeClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
@@ -67,32 +51,3 @@ for (var i = 0; i < btns.length; i++) {
 }
 filterSelection("all")
 
-
-// function showProjDescription(event){
-//     event.innerHTML = 'yayyyy';
-//     event.lastchild.style.display = 'block';
-//     event.firstchild.style.display = 'none';
-//     // proj.getElementsByClassName('proj-image').style.display = 'default';
-//     // proj.getElementsByClassName('proj-description').style.display = 'none';
-// };
-
-// for (var i = 0; i < projs.length; i += 1) {
-//     projs[i].style.color = black;
-//     showProjDescription();
-// }
-
-// proj2.onclick = showProjDescription;
-// proj3.onmouseover = function(){
-//     // let content = proj3.lastChild.text;
-//     let image = proj3.firstChild; 
-//     image.style.display = 'none';
-//     image.nextSibling.style.display = 'inline';
-
-//     // proj3.lastChild.style.display = 'block';
-//     // proj.getElementsByClassName('proj-image').style.display = 'default';
-//     // proj.getElementsByClassName('proj-description').style.display = 'none';
-// };
-
-// itemOne.onmouseover = function(){
-//     itemOne.style.width = '430px';
-//   };
